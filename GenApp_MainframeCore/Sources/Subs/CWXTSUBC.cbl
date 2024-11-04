ba22b2adc2849b0a72953957ebb2687d34f6dec5
Manoj Singh <manoj_singh@bmc.com>
2024-11-04 11:18:03 -0600

    #3

--- GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
+++ GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
@@ -12,7 +12,7 @@
       *  BASED ON MONTHLY SALES RATES                                  *
       *                                                                *
       *                                                                *
-      * DEMO 24.01.2024 12:02                                          *
+      * DEMO 24.01.2024 12:03                                          *
       ******************************************************************
        ENVIRONMENT DIVISION.
        DATA DIVISION.
